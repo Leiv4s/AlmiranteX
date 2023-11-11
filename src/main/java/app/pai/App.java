8d1d401ba7a70b546ac7657c11a71fa9b9c7783d
leiv4s <leivashenrique12@gmail.com>
2023-11-11 13:09:02 -0300

    Implementando o fxml da tela de Inicio

--- src/main/java/app/pai/App.java
+++ src/main/java/app/pai/App.java
@@ -11,7 +11,7 @@ public class App extends Application{
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/Fxml/ComponentMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/Fxml/Inicio.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 276, 650);
         stage.setTitle("Hello!");
         stage.setScene(scene);
