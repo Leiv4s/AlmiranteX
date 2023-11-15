61edb57e73cbde13fdd2b630291d96c4802bbb5b
leiv4s <leivashenrique12@gmail.com>
2023-11-15 19:35:32 -0300

    Construção da pagina Editar Loja

---
+++ src/main/java/app/pai/models/Categoria.java
@@ -0,0 +1,13 @@
+package app.pai.models;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+
+public class Categoria {
+
+    private ArrayList<String> categorias= new ArrayList<>();
+
+
+
+
+}
