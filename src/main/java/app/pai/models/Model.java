1a7b0186f82307a5b29d85cf637c04155657b6b1
leiv4s <leivashenrique12@gmail.com>
2023-11-16 21:47:03 -0300

    implementação da classe Model, funciona controlador de instancia do ViewFactory

--- src/main/java/app/pai/models/Model.java
+++ src/main/java/app/pai/models/Model.java
@@ -1,4 +1,29 @@
 package app.pai.models;
 
+import app.pai.views.ViewFactory;
+
 public class Model {
+/*
+This class has an important function that is make sure that only one instance of the viewfactory
+object is operating the screens of the project, protecting the system of having more than one
+instances building and operating screens without need.
+ */
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+
+    private Model(){
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
