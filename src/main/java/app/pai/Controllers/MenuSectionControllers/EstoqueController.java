6538cc39fae73c1465eb696c22bf028e7d3b11ef
leiv4s <leivashenrique12@gmail.com>
2023-12-10 21:30:34 -0300

    inicio da construção da pagina Estoque.fxml

--- src/main/java/app/pai/Controllers/MenuSectionControllers/EstoqueController.java
+++ src/main/java/app/pai/Controllers/MenuSectionControllers/EstoqueController.java
@@ -1,4 +1,8 @@
 package app.pai.Controllers.MenuSectionControllers;
 
+import javafx.event.ActionEvent;
+
 public class EstoqueController {
+    public void criarProdutoBtnOnClick(ActionEvent event) {
+    }
 }
